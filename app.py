fb39ecb80ad669633c73a0bedabdf134efffcc9d
loyata <77038994+loyata@users.noreply.github.com>
2023-03-30 11:52:53 -0400

    new vers

--- app.py
+++ app.py
@@ -6,33 +6,19 @@ import matplotlib.pyplot as plt
 import io
 import base64
 import mysql.connector
+import socket
 
+import random
+
+import matplotlib
+matplotlib.use('Agg')
 
 
 
 app = Flask(__name__)  # See here
-# app.config['MYSQL_HOST'] = 'localhost' # Replace with your host
-# app.config['MYSQL_USER'] = 'root' # Replace with your MySQL username
-# app.config['MYSQL_PASSWORD'] = '123456' # Replace with your MySQL password
-# app.config['MYSQL_DB'] = 'eHospital' # Replace with your MySQL database name
-
-
-# app.config['MYSQL_HOST'] = 'us-cdbr-east-06.cleardb.net' # Replace with your host
-# app.config['MYSQL_USER'] = 'b05e8c26aca707' # Replace with your MySQL username
-# app.config['MYSQL_PASSWORD'] = 'b1ead699' # Replace with your MySQL password
-# app.config['MYSQL_DB'] = 'heroku_6f03eb70d098e57' # Replace with your MySQL database name
-
-
 
 # mysql = MySQL(app)
 CORS(app, resources={r"/*": {"origins": "*"}})
-
-# cnx = mysql.connector.connect(
-#     host='us-cdbr-east-06.cleardb.net',
-#     user='b05e8c26aca707',
-#     password='b1ead699',
-#     database='heroku_6f03eb70d098e57'
-# )
 
 
 @app.route("/get_my_ip", methods=["GET"])
@@ -74,14 +60,15 @@ def graph():
     # response.headers['Content-Type'] = 'text/html'
     return response
 
+
 @app.route("/predict_liver_disease", methods=["POST"])
 @cross_origin()
 def predict_liver_disease():
     cnx = mysql.connector.connect(
-        host='us-cdbr-east-06.cleardb.net',
-        user='b05e8c26aca707',
-        password='b1ead699',
-        database='heroku_6f03eb70d098e57'
+        host='frwahxxknm9kwy6c.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+        user='j6qbx3bgjysst4jr',
+        password='mcbsdk2s27ldf37t',
+        database='nkw2tiuvgv6ufu1z'
     )
     data = request.json
     print(data)
@@ -100,6 +87,9 @@ def predict_liver_disease():
             1 if data['gender'] == 'female' else 0,
             1 if data['gender'] == 'male' else 1
         ]]
+
+        print(input)
+
         res = loaded_model.predict(input)[0]
 
         # cur = mysql.connection.cursor()
@@ -153,5 +143,100 @@ def predict_MS():
     print(diagnose)
     return diagnose
 
+
+@app.route("/get_all_liver_data", methods=["GET"])
+@cross_origin()
+def get_all_liver_data():
+    cnx = mysql.connector.connect(
+        host='frwahxxknm9kwy6c.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+        user='j6qbx3bgjysst4jr',
+        password='mcbsdk2s27ldf37t',
+        database='nkw2tiuvgv6ufu1z'
+    )
+    cursor = cnx.cursor()
+    query = "SELECT * FROM liver"
+    cursor.execute(query)
+    results = cursor.fetchall()
+    return results
+
+@app.route("/predict_MS_direct/<int:id>", methods=["GET"])
+@cross_origin()
+def predict_MS_direct(id):
+    MS_model = pickle.load(open('model', 'rb'))
+    cnx = mysql.connector.connect(
+        host='frwahxxknm9kwy6c.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+        user='j6qbx3bgjysst4jr',
+        password='mcbsdk2s27ldf37t',
+        database='nkw2tiuvgv6ufu1z'
+    )
+    cursor = cnx.cursor()
+
+    query = "SELECT pyramidal, cerebella, brain_stem, sensory, visual, mental, bowel_and_bladder_function, mobility " \
+            "FROM physical_test_ms WHERE patient_id = %s"
+    cursor.execute(query, (id,))
+
+    data = cursor.fetchone()
+    index = ["0"]
+    columns = ['Pyramidal', 'Cerebella', 'Brain stem', 'Sensory', 'Visual', 'Mental', 'Bowel and bladder function',
+               'Mobility']
+
+    # Create a pandas DataFrame with the specified row and column names
+    df = pd.DataFrame([data], index=index, columns=columns)
+    X = df
+    print(X)
+    # Make predictions on the test data
+    y_pred = MS_model.predict(X)
+    if (y_pred == 1):
+        diagnose = 'patient suffers from MS'
+    elif (y_pred == 0):
+        diagnose = 'patient does not suffer from MS'
+    print(diagnose)
+    return diagnose
+
+
+
+@app.route("/predict_liver_disease_direct/<int:id>", methods=["GET"])
+@cross_origin()
+def predict_liver_disease_direct(id):
+    cnx = mysql.connector.connect(
+        host='frwahxxknm9kwy6c.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+        user='j6qbx3bgjysst4jr',
+        password='mcbsdk2s27ldf37t',
+        database='nkw2tiuvgv6ufu1z'
+    )
+    cursor = cnx.cursor()
+    loaded_model = pickle.load(open('finalized_model.sav', 'rb'))
+    query = "SELECT name, age, gender, total_bilirubin, direct_bilirubin, " \
+            "alkaline_phosphatase, alanine_aminotransferase, aspartate_aminotransferase, " \
+            "total_proteins, albumin, albumin_and_globulin_ratio " \
+            "FROM liver " \
+            "WHERE patient_id = %s"
+    cursor.execute(query, (id,))
+
+    result = cursor.fetchone()
+
+    input = [[
+        float(result[1]),
+        float(result[3]),
+        float(result[4]),
+        float(result[5]),
+        float(result[6]),
+        float(result[7]),
+        float(result[8]),
+        float(result[9]),
+        float(result[10]),
+        1 if result[2] == 'female' else 0,
+        1 if result[2] == 'male' else 1
+    ]]
+    res = loaded_model.predict(input)[0]
+    return "Potential liver disease detected, further examination required." if res == 1 else "No liver disease detected."
+
+
+
+
 if __name__ == '__main__':
-    app.run()
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.bind(('', 0))
+    port = sock.getsockname()[1]
+    sock.close()
+    app.run(debug=True, port=port)
